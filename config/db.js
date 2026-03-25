91a00904152af1c03447a4b9d8730eb55d60565c
Elima Gezhilli <gezhillielima96@gmail.com>
2026-03-25 18:02:19 +0100

    rregullim final per render

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Kjo rresht duhet patjetër lart
 
 const pool = mysql.createPool({
-    host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',     
-    user: '4hdF44ve6Gs6wEe.root',          
-    password: 'mpkGypkTOf3xPVzM',    
-    database: 'test',        
-    port: 4000,                   
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,          
+    password: process.env.DB_PASS,    
+    database: process.env.DB_NAME,        
+    port: process.env.DB_PORT || 4000,                   
     ssl: {
         rejectUnauthorized: true  
     }
