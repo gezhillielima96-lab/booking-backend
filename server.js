b015c6cfa34be8720604732356555bf4fd2a64a4
Elima Gezhilli <gezhillielima96@gmail.com>
2026-03-25 18:22:03 +0100

    dërgimi final per render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+
 
 // Thirrja e databazës
 const db = require('./config/db'); 
@@ -22,8 +22,10 @@ db.query('SELECT 1')
     .then(() => console.log("✅ LIDHJA ME TIDB ONLINE U KRYE ME SUKSES!"))
     .catch(err => console.log("❌ GABIM NE LIDHJEN ME TIDB:", err));
 
-// --- NDRYSHIMI PER RENDER ---
-const PORT = process.env.PORT || 10000; // Render shpesh përdor 10000 ose variablin e tij
+// --- KONFIGURIMI PER RENDER (SHUME I RENDESISHEM) ---
+const PORT = process.env.PORT || 10000; 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Serveri është LIVE në portin ${PORT}`);
+    console.log(`🚀 Serveri është LIVE në portin ${PORT}`);
 });
+
+// Test final per Git 2026
