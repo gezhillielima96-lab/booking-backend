40e16f9f7503ae60fedad5c4f92003b6eb5cf406
Elima Gezhilli <gezhillielima96@gmail.com>
2026-03-25 20:13:00 +0100

    Shtimi i start script per Render

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // Kjo rresht duhet patjetër lart
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER,          
-    password: process.env.DB_PASS,    
-    database: process.env.DB_NAME,        
-    port: process.env.DB_PORT || 4000,                   
-    ssl: {
-        rejectUnauthorized: true  
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true 
+  }
 });
 
 module.exports = pool.promise();
